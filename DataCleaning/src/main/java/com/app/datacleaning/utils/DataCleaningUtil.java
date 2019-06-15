6b5608389ed2d8b90be669f18fd2ae1975d8f522
zhouyang <zhouyangzyi@live.com>
2019-06-15 12:12:10 +0800

    添加gitignore文件

---
+++ DataCleaning/src/main/java/com/app/datacleaning/utils/DataCleaningUtil.java
@@ -0,0 +1,97 @@
+/**
+ * @ClassName DataCleaningUtil
+ * @Description TODO
+ * @Author zy
+ * @Date 2019/6/15 12:04
+ * @Version 1.0
+ **/
+package com.app.datacleaning.utils;
+
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+/*
+ * @Author zhouyang
+ * @Description //TODO https://github.com/lihongcang/GeneralDataFormator/blob/master/src/main/java/Producer.java
+ * 参考地址：https://github.com/lihongcang/GeneralDataFormator/blob/master/src/main/java/Producer.java
+ * @Date 12:04 2019/6/15
+ * @Param
+ * @return
+ **/
+public class DataCleaningUtil {
+    private static boolean remove_whitespace = true;
+    private static boolean remove_not_ch_en_num = true;
+    private static int repeat2one = 6;
+    private static boolean remove_repeat_if_whitespace = false;
+    private static boolean remove_html_tag = true;
+    private static int length_limit = 5;
+
+    public DataCleaningUtil(String clean_data_config_path){
+
+        //读取cleanData的配置文件
+        Properties properties = new Properties();
+        try (InputStream input = Producer.class.getResourceAsStream(clean_data_config_path)) {
+            properties.load(input);
+            remove_whitespace = Boolean.valueOf(properties.getProperty("remove_whitespace"));
+            remove_not_ch_en_num = Boolean.valueOf(properties.getProperty("remove_not_ch_en_num"));
+            repeat2one = Integer.valueOf(properties.getProperty("repeat2one"));
+            remove_repeat_if_whitespace = Boolean.valueOf(properties.getProperty("remove_repeat_if_whitespace"));
+            remove_html_tag = Boolean.valueOf(properties.getProperty("remove_html_tag"));
+            length_limit = Integer.valueOf(properties.getProperty("length_limit"));
+        } catch (Exception e) {
+            System.out.println("error appeared when read the config file");
+            e.printStackTrace();
+        }
+
+    }
+    public static String cleanData(String content){
+        //去除空格
+        if(remove_whitespace){
+            content = content.replaceAll("\\s","");
+        }
+        //去除重复字符
+        if(remove_repeat_if_whitespace){
+            content = content.replaceAll("(.)\\1{"+repeat2one+",}","$1");
+        }else{
+            content = content.replaceAll("([^\\s])\\1{"+repeat2one+",}","$1");
+        }
+
+        //去除乱码
+        if(remove_not_ch_en_num){
+            content = content.replaceAll("[[^\u4E00-\u9FA5]&&[^a-zA-Z0-9\\s]&&[^~`!@#$%^&*()\\-_+={}\\[\\]|\\:;\"\'<>,./]&&[^~?！@#￥…（）?+={}【】、：；“”‘ ’《》，。？]]", "");
+        }
+
+        //去除html标签
+        if(remove_html_tag){
+            content = content.replaceAll("<script.*?>[\\s\\S]*?<.*?/script>","");
+            content = content.replaceAll("&nbsp","");
+            content = content.replaceAll("<[^>\\d]+.*?>","");
+        }
+        //去除长度小于100
+        if( content.replaceAll("\\s","").length()<length_limit){
+            return null;
+        }
+        return content;
+    }
+
+    public static void main(String[] args){
+        String content = "《上 海金 融 ）２０１４年 第 １期　\n" +
+                "。。　。。　。。　。。　。。。　。。　－。。　。。　。。　。。。。。。。　。。’。。’。。。。。’。。’。。’。。’。。　。。’　。’。。’。。’。。’。。’。。　。。　。。’。。　。。。　。。　。。　。。。。。　。。　。。’。。’。。’。。’。。’。。’。。’。。　。。；　\n" +
+                "货币政策对股票收益率的非对称性影响研究 术　\n" +
+                "０：．。。．◆　（）。．　）．◆。。．◆ｎ。。．。。◆．。。．。。．。。．。。．。。．◆。。．ｃ。。◆．。。．　ｕ◆．。。．◆。。．◆。。◆．。。◆．。。．◆。。．。。．　（ｊ。．◆。。．。（）　（，）◆．。。．。。．◆。。．◆。。◆．。。◆．。。．。。．。　（）．　、。◆．　（］◆．。。．◆。。◆．。。．。。．。。．　（　．　．　（　、（、．．”．、．◆　。；：　\n" +
+                "毛 泽 盛　，潘 吟 斐　\n" +
+                "１，２ 南 京 师 范 大 学 商 学 院 ，　 江 苏                  南京 ２１００２３）　";
+        content = content.replaceAll("([^\\s])\\1{5,}","$1");    //去三个及以>上叠词为一个词
+
+        content = content.replaceAll("[[^\u4E00-\u9FA5]&&[^a-zA-Z0-9\\s]&&[^~`!@#$%^&*()\\-_+={}\\[\\]|\\:;\"\'<>,./]&&[^~?！@#￥…（）?+={}【】、：；“”‘ ’《》，。？]]", "");    //除去乱码
+
+        //content = content.replaceAll("<script.*?>[\\s\\S]*?<.*?/script>","");
+        content = Pattern.compile("<script.*?>.*?</script>",Pattern.DOTALL).matcher(content).replaceAll("");
+        content = Pattern.compile("<style.*?>.*?</style>",Pattern.DOTALL).matcher(content).replaceAll("");
+        content = content.replaceAll("&nbsp","");
+        content = content.replaceAll("<[^>\\d]+.*?>","");
+        System.out.println(content);
+
+    }
+}
---
+++ DataCleaning/src/main/java/com/app/datacleaning/utils/Producer.java
@@ -0,0 +1,184 @@
+/**
+ * @ClassName Producer
+ * @Description TODO
+ * @Author zy
+ * @Date 2019/6/15 12:06
+ * @Version 1.0
+ **/
+package com.app.datacleaning.utils;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+
+public class Producer implements Runnable{
+
+    private BlockingQueue<Map<String,String>> blockingQueue;
+    private Properties properties = new Properties();
+
+    //数据源类型
+    private String type;
+    //如果数据源是数据库，配置连接参数
+    private String driverName, dbURL, userName, userPwd,psm;
+    private Connection dbConn = null;
+    private PreparedStatement sm = null;
+    private ResultSet rs = null;
+    private String[] fields;
+    //如果数据源是文本，配置路径参数
+    private String file_path;
+    private String n2one;
+
+
+    public Producer(BlockingQueue<Map<String, String>> blockingQueue,String producer_config_path) {
+        try (InputStream input = Producer.class.getResourceAsStream(producer_config_path)) {
+            properties.load(input);
+            this.blockingQueue = blockingQueue;
+            type = properties.getProperty("type");
+
+            switch (type){
+                case "database":
+                    System.out.println("producer is reading your database config");
+                    driverName = properties.getProperty("jdbc.driver");
+                    dbURL = properties.getProperty("jdbc.url");
+                    userName = properties.getProperty("jdbc.user");
+                    userPwd = properties.getProperty("jdbc.pass");
+                    psm = properties.getProperty("jdbc.statement");
+                    String fields_string = properties.getProperty("jdbc.fields_string");
+                    fields = fields_string.split(":");
+                    break;
+                case "text":
+                    System.out.println("producer is reading your text config");
+                    file_path = properties.getProperty("file_path");
+                    n2one=properties.getProperty("n2one");
+                    break;
+                default:
+                    System.out.println("please config your data source type in config_producer.properties");
+                    break;
+            }
+
+        } catch (Exception e) {
+            System.out.println("error appeared when read the config file");
+            e.printStackTrace();
+        }
+
+    }
+
+    public void run() {
+        switch (type){
+            case "database":
+                getFromDB();
+                break;
+            case "text":
+                getFromText();
+        }
+    }
+
+    public void getFromDB(){
+        DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        int i = 0;
+
+        //连接数据库
+        try {
+            Class.forName(driverName);
+            dbConn = DriverManager.getConnection(dbURL, userName, userPwd);
+            System.out.println("connect successfully!");
+        } catch (Exception e) {
+            System.out.println("connect failed!");
+            e.printStackTrace();
+        }
+
+        //取数据放入队列
+        try {
+            sm = dbConn.prepareStatement(psm);
+            rs = sm.executeQuery();
+            while (rs.next()) {
+                Map<String, String> row = new HashMap<>();
+                for(String field : fields){
+                    row.put(field,rs.getString(field));
+                }
+                blockingQueue.put(row);
+
+                //打印时间
+                i += 1;
+                if (i % 1000 == 0) {
+                    System.out.println("producer\t"+fmt.format(System.currentTimeMillis()) +"=> " + i);
+                }
+            }
+
+            //查询结束标志
+            Map<String, String> row = new HashMap<>();
+            for(String field : fields){
+                row.put(field,"end-end-end");
+            }
+            blockingQueue.put(row);
+            System.out.println("query is over");
+            dbConn.close();
+
+        } catch (Exception e) {
+            System.out.println("query database failed!");
+            e.printStackTrace();
+        }
+    }
+
+    public void getFromText(){
+        DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        int i = 0;
+
+        InputStreamReader read;// 考虑到编码格式
+        try {
+            read = new InputStreamReader(new FileInputStream(file_path), "UTF-8");
+            BufferedReader bufferedReader = new BufferedReader(read);
+            String lineTxt;
+
+            int count=1;
+            String content="";
+            while ((lineTxt = bufferedReader.readLine()) != null) {
+                if(count < Integer.valueOf(n2one)){
+                    content+=lineTxt+"\n";
+                    count+=1;
+                    continue;
+                }
+
+                Map<String, String> row = new HashMap<>();
+                row.put("content",content);
+                blockingQueue.put(row);
+                count=1;
+                content="";
+
+                i = i+1;
+                if(i%1000 == 0){
+                    System.out.println("producer\t"+fmt.format(System.currentTimeMillis()) +"=> " + i);
+                }
+            }
+            if(content!=""){
+                Map<String, String> row = new HashMap<>();
+                row.put("content",content);
+                blockingQueue.put(row);
+            }
+
+            //查询结束标志
+            Map<String, String> row = new HashMap<>();
+            row.put("content","end-end-end");
+            blockingQueue.put(row);
+
+            bufferedReader.close();
+            read.close();
+
+        } catch (Exception e) {
+            System.out.println("producer read from text failed");
+            e.printStackTrace();
+        }
+
+    }
+}
